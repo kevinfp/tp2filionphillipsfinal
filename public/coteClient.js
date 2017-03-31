1c5faf714c70336826b716177da7a15b9dc03647
TiM <TiM>
2017-03-30 21:16:32 -0400

    La jen suis au point de faire le supprimer dans le tableau directement. le modifier est fait et marche bien.

--- public/coteClient.js
+++ public/coteClient.js
@@ -1,26 +1,48 @@
-"use strict"
+"use strict";
 (()=>{
 
-document.getElementsByClassName("modifier");
-document.getElementsByClassName("supprimer");
+var modifBtn = document.getElementsByClassName("modifier");
+var suppBtn = document.getElementsByClassName("supprimer");	
+var nomModif = document.getElementsByClassName("nomMod")[0];
+var prenomModif = document.getElementsByClassName("prenomMod")[0];
+var telephoneModif = document.getElementsByClassName("telephoneMod")[0];
+var villeModif = document.getElementsByClassName("villeMod")[0];
+var groupeSanguinModif = document.getElementsByClassName("groupeSanguinMod")[0];
+var idDeModif = document.getElementsByClassName("idMod")[0];
+
+var formulaireModif = document.getElementById('formMod');
 
 
+//console.log(modifBtn.length);
+
+for(var i=0; i<modifBtn.length; i++)
+{
+	modifBtn[i].addEventListener("click", modifierFonction);
+}
+
+for(var i=0; i<suppBtn.length; i++)
+{
+	suppBtn[i].addEventListener("click", supprimerFonction);
+}
 
 
+function modifierFonction() 
+{
+  nomModif.value = this.parentElement.parentElement.getElementsByTagName('td')[0].innerHTML;
+  prenomModif.value = this.parentElement.parentElement.getElementsByTagName('td')[1].innerHTML;
+  telephoneModif.value = this.parentElement.parentElement.getElementsByTagName('td')[2].innerHTML;
+  villeModif.value = this.parentElement.parentElement.getElementsByTagName('td')[3].innerHTML;
+  groupeSanguinModif.value = this.parentElement.parentElement.getElementsByTagName('td')[4].innerHTML;
+  idDeModif.value = this.parentElement.parentElement.getElementsByTagName('td')[5].innerHTML;
 
+  formulaireModif.submit();
+  //console.log(nomModif);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function supprimerFonction() 
+{
+  console.log(this + 'SUPPRIMER');  
+}
 
 
 
